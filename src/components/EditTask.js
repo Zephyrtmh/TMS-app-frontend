87731a2a4dc17d5f8343411af426d8f84e0d90f9
Zephyr <Zephyrtmh@gmail.com>
2023-07-09 18:39:16 +0800

    changes

--- src/components/EditTask.js
+++ src/components/EditTask.js
@@ -1,28 +1,35 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import { useNavigate, useParams, useLocation } from "react-router-dom";
 import axios from 'axios'
 import AppStateContext from '../AppStateContext';
 
-export default function EditTask({editType}) {
+export default function EditTask() {
 
     const { taskId } = useParams();
     const location = useLocation();
+    const stateData = location.state;
 
-    const searchParams = new URLSearchParams(location.search);
-    const type = searchParams.get('type');
+    // const searchParams = new URLSearchParams(location.search);
+    // const type = searchParams.get('type');
 
     const appState = useContext(AppStateContext);
 
-    [task, setTask] = useState({});
+    const [task, setTask] = useState({});
 
 
 
     useEffect(() => {
         //get task details from backend
-        axios.post(`http://localhost:8080/task/${taskId}`, {verification:  { username: appState.username, userGroupsPermitted: [], isEndPoint: false } })
+        console.log(location)
+        // try {
+        //     axios.post(`http://localhost:8080/task/${taskId}`, {verification:  { username: appState.username, userGroupsPermitted: [], isEndPoint: false } })
+        // }
+        // catch(err) {
+        //     console.log(err)
+        // }
     })
 
   return (
-    <div>{"taskId " + taskId + type}</div>
+    <div>{"taskId " + taskId +location.state}</div>
   )
 }
--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,11 +1,13 @@
 import "../styles/taskcard.css";
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function TaskCard(task) {
 
+    const navigate = useNavigate();
+
     const handlePromoteButtonClick = (taskId) => {
-        navigate(`/task/${taskId}?type=promote`);
+        navigate(`/task/${taskId}?type=promote`, {state: "test"});
     };
 
     const handleEditButtonClick = (taskId) => {
@@ -16,8 +18,6 @@ export default function TaskCard(task) {
         navigate(`/task/${taskId}?type=demote`);
     };
 
-    const navigate = useNavigate();
-
     return (
         <div className="task-card-container">
             <div className="colour-bar" style={{ backgroundColor: task.plan_colour }}></div>
@@ -26,6 +26,9 @@ export default function TaskCard(task) {
             <div>{task.task_owner}</div>
             <div>{task.task_createdate}</div>
             <button onClick={() => {handleDemoteButtonClick(task.task_id)}}>Demote</button>
+            
+                {/* <Link to={{ pathname:`/task/${task.task_id}`,search: "?type=demote"}} state={task}><button>Demote</button></Link> */}
+            
             <button onClick={() => {handleEditButtonClick(task.task_id)}}>Edit</button>
             <button onClick={() => {handlePromoteButtonClick(task.task_id)}}>Promote</button>
         </div>
